a9410abec0c4e4c4849d3a85081a59be36ea7ab3
Zhen Jun Xu <arlenxuzj@gmail.com>
2023-05-12 12:27:38 -0400

    feat: remove google analytics

--- src/configs/analyticsConfig.ts
+++ src/configs/analyticsConfig.ts
@@ -1,6 +1,6 @@
 const analyticsConfig = {
   vercel: true,
-  google: true, // process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
+  google: false, // process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
   umami: true, // process.env.NEXT_PUBLIC_UMAMI_ID
   umamiUrl: 'https://umami.arlenx.io/script.js'
 };
