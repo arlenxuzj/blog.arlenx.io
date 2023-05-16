089bc0b1c30a4bddd7eb8eaf6689dddd5a179dcc
Zhen Jun Xu <arlenxuzj@gmail.com>
2023-05-16 01:08:08 -0400

    fix: update sitemap

--- scripts/sitemap.ts
+++ scripts/sitemap.ts
@@ -1,63 +1,120 @@
 import fs from 'fs';
 
+import { format as formatFn, parseISO } from 'date-fns';
 import { globbySync } from 'globby';
 import matter from 'gray-matter';
 import _ from 'lodash';
 import prettier from 'prettier';
+import { exec } from 'shelljs';
 
-import navLinks from '@/configs/navLinks';
 import siteMeta from '@/configs/siteMeta';
 
 console.log('Generating sitemap...');
 
-const pages = globbySync(
-  ['pages/*.tsx', '!pages/_*.tsx', '!pages/api', '!pages/404.tsx'],
+const getLastModifiedDateFromGit = (filename: string) => {
+  const stdout = exec(`git log -1 --pretty="format:%cI" ${filename}`, {
+    silent: true
+  }).stdout;
+
+  return formatFn(parseISO(stdout), 'yyyy-MM-dd');
+};
+
+const pagePaths = globbySync(
+  [
+    'src/pages/**/*.tsx',
+    '!src/pages/_*.tsx',
+    '!src/pages/api',
+    '!src/pages/404.tsx'
+  ],
   {
     gitignore: true
   }
-);
+)
+  .filter(path => path.indexOf('[slug]') === -1)
+  .map(path => ({
+    path,
+    lastModified: getLastModifiedDateFromGit(path)
+  }));
 
-const pageUrls = pages.map(page =>
-  page.replace('pages', '').replace('.tsx', '').replace('/index', '/')
-);
+console.log(pagePaths);
 
-const navLinkUrls = navLinks.map(navLink => navLink.href);
+const pageUrls = pagePaths.map(pagePath => ({
+  ...pagePath,
+  url: pagePath.path
+    .replace('src/pages', '')
+    .replace('/index', '')
+    .replace('.tsx', '')
+}));
 
-const blogContent = globbySync(['contents/**/*.mdx', '!contents/pages'], {
+console.log(pageUrls);
+
+const blogContentPaths = globbySync(['contents/**/*.mdx'], {
   gitignore: true
 }).filter(content => {
   const file = fs.readFileSync(content, 'utf-8');
   const { data } = matter(file);
-
-  return !data.wip;
+  return !data.ignore;
 });
 
-const blogContentUrls = blogContent.map(content =>
-  content.replace('contents', '').replace('.mdx', '')
+const blogContentUrls = blogContentPaths.map(contentPath => {
+  const file = fs.readFileSync(contentPath, 'utf-8');
+  const { data } = matter(file);
+
+  return {
+    path: contentPath,
+    lastModified: formatFn(data.updatedAt || data.createdAt, 'yyyy-MM-dd'),
+    url: contentPath
+      .replace('contents', '')
+      .replace('.mdx', '')
+      .replace('/pages', '')
+  };
+});
+
+console.log(blogContentUrls);
+
+const pathUrlsWithoutMDX = pageUrls.filter(pageUrl => {
+  return !blogContentUrls.find(item => item.url === pageUrl.url);
+});
+
+console.log(pathUrlsWithoutMDX);
+
+const tagsUrls = _.uniqBy(
+  _.orderBy(
+    _.flatten(
+      blogContentPaths.map(content => {
+        const file = fs.readFileSync(content, 'utf-8');
+        const { data } = matter(file);
+
+        return data.tags?.map((tag: string) => ({
+          lastModified: formatFn(
+            data.updatedAt || data.createdAt,
+            'yyyy-MM-dd'
+          ),
+          url: `/tags/${tag}`
+        }));
+      })
+    ).filter(Boolean),
+    'lastModified',
+    'desc'
+  ),
+  'url'
 );
 
-const tagsUrls = _.uniq(
-  _.flatten(
-    blogContent.map(content => {
-      const file = fs.readFileSync(content, 'utf-8');
-      const { data } = matter(file);
+console.log(tagsUrls);
 
-      return data.tags.map((tag: string) => `/tags/${tag}`);
-    })
-  )
-);
-
-const paths = _.uniq([
-  ...pageUrls,
-  ...navLinkUrls,
+const urlData: { url: string; path?: string; lastModified: string }[] = [
+  ...pathUrlsWithoutMDX,
   ...blogContentUrls,
   ...tagsUrls
-]).sort();
+];
 
-const urlTags = paths
-  .map(path => {
+console.log(urlData);
+
+const urlTags = urlData
+  .map(data => {
     return `<url>
-      <loc>${`${siteMeta.url}${path}`}</loc>
+      <loc>${`${siteMeta.url}${data.url}`}</loc>
+      <lastmod>${data.lastModified}</lastmod>
     </url>
     `;
   })
