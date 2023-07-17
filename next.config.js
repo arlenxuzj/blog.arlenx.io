041f1e1298aaaf3ef08cfa0418ad686a834e38dd
Zhen Jun Xu <arlenxuzj@gmail.com>
2023-07-17 05:06:58 -0700

    chore: rename next.config.mjs to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import { withContentlayer } from 'next-contentlayer';
+const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -28,4 +28,4 @@ const nextConfig = {
   }
 };
 
-export default withContentlayer(nextConfig);
+module.exports = withContentlayer(nextConfig);
